f98b664f7a16dc960b38593e78acb82cba8a5f43
Samantha Lurio <samantha.lurio@gmail.com>
2020-06-12 23:46:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artistName: "songTitle"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  newPlaylist = Object.assign({},playlist, {artistName: "songTitle"});
+  newPlaylist = Object.assign({},playlist, {[artistName]: songTitle});
   return newPlaylist;
 }
