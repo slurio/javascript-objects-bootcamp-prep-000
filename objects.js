1170e5afc914be2b1e4f17cfb591de6c6abcbd73
Samantha Lurio <samantha.lurio@gmail.com>
2020-06-12 23:39:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,1 @@
 var playlist = { "Slowdive": "Allison","My Bloody Valentine" : "Sometimes"};
-
-function updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi") {
-  return Object.assign({}, playlist, {[Phil Ochs]: "Here's to the State of Mississippi"});
-}
