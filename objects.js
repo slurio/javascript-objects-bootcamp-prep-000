1ff6450aed967d7031e477eca9984e7de4b597c7
Samantha Lurio <samantha.lurio@gmail.com>
2020-06-12 23:48:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,2 @@
 var playlist = { artistName: "songTitle"};
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  var newPlaylist = Object.assign({},playlist, {[Phil Ochs]: songTitle});
-  return newPlaylist;
-}
