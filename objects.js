5a3a2c9acc4c2f77cad410cdd57a1696a525874d
Samantha Lurio <samantha.lurio@gmail.com>
2020-06-12 23:57:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,11 +3,9 @@ var playlist = { artistName: "songTitle"};
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, {artistName: songTitle});
   return playlist;
-} 
+}
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
-
-
